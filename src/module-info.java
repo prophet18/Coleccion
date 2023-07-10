da4c71ecbabbcfc13a2b2f6dbe977fb3fb57ecad
Elliot D <prophet.18@rbx.life>
2023-07-10 12:29:14 -0400

    event handlers new
    new eh

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,19 @@
 /**
  * @author deane
+ * 
+ * Notes
+ * 
+ * 
+ * Filepath for JavaFX on Grendel -
+ * Filepath for JavaFX on Cronus - /usr/lib/jvm/java-20-openjdk/
+ * Filepath for images on Cronus - /home/cronus/GitHub/Coleccion/bamboo_scroll_art_2.jpg
+ * Filepath for images on Grendel - C://Users//deane//GitHub//Coleccion//bamboo_scroll_art_2.jpg
+ * 
+ * Filepath for CSS in Cronus - /home/cronus/GitHub/Coleccion/css_styling_coleccion.css
+ * 
  *
  */
+
 module Colección {
 	requires javafx.graphics;
 	requires javafx.controls;
